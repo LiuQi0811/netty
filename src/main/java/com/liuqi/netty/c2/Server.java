254019c3edec12fef428ae7be78c7004958da71b
LIUQI <1139642444@qq.com>
2023-02-05 11:49:24 +0800

    服务端 socketChannelSelector

--- src/main/java/com/liuqi/netty/c2/Server.java
+++ src/main/java/com/liuqi/netty/c2/Server.java
@@ -5,11 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /*
  *@ClassName Server
@@ -21,7 +25,7 @@ import java.util.List;
 @Slf4j
 public class Server {
     public static void main(String[] args) throws IOException {
-        socketChannelBlocking();
+        socketChannelSelector();
     }
 
     /**
@@ -110,8 +114,68 @@ public class Server {
                     log.info("数据读取结束......  {}", channel);
                 }
             }
+        }
+    }
 
-
+    /**
+     *  socketChannelSelector
+     * @throws IOException
+     */
+    private static void socketChannelSelector() throws IOException {
+        // 创建 selector 对象 可以管理多个channel
+        final Selector selector = Selector.open();
+        // 创建缓冲区
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(30);
+        // 创建server socket 服务
+        final ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 设置非阻塞模式
+        serverSocketChannel.configureBlocking(false);
+        // 创建 selector 和 channel的联系 （注册）
+        // SelectionKey 将来事件发生后 通过他可以知道事件和哪个channel的事件   0代表不关注任何事件
+        final SelectionKey serverSocketKey = serverSocketChannel.register(selector, 0, null);
+        log.info("serverSocketKey: {}",serverSocketKey);
+        // SelectionKey 只关注 ACCEPT事件
+        serverSocketKey.interestOps(SelectionKey.OP_ACCEPT);
+        // 绑定端口号
+        serverSocketChannel.bind(new InetSocketAddress(8080));
+        while (true){
+            // select 方法 没有事件发生 线程阻塞 有事件发生线程恢复运行
+            selector.select();
+            // 处理事件 selectedKeys可读可写连接事件集合 内部包含了所有发生的事件
+            final Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            // 遍历数据
+            final Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()){
+                //  通过iterator 获取SelectionKey
+                final SelectionKey selectionKey = keyIterator.next();
+                // 删除SelectionKey 必须 从selectionKeys 集合中删除，否则下次处理就有问题报错
+                keyIterator.remove();
+                log.info("selectionKey: {}",selectionKey);
+                // 区分事件类型
+                if (selectionKey.isAcceptable()){ // accept事件  ServerSocketChannel触发的
+                    // 创建server socket服务
+                    final ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();// 拿到触发事件的channel
+                    // 创建服务连接  accept建立与客户端连接 tcp 三次握手  SocketChannel用来与客户端之间通信
+                    final SocketChannel socketChannel = channel.accept();
+                    log.info("{}",socketChannel);
+                    // 非阻塞模式
+                    socketChannel.configureBlocking(false);
+                    final SelectionKey socketKey = socketChannel.register(selector, 0, null);
+                    socketKey.interestOps(SelectionKey.OP_READ);
+                    log.info("socketKey {}",socketKey);
+                }else if (selectionKey.isReadable()){ // read 可读事件 SocketChannel 读取
+                    final SocketChannel channel = (SocketChannel)selectionKey.channel(); // 拿到触发事件的channel
+                    // 创建缓冲区
+                    final ByteBuffer buffer = ByteBuffer.allocate(30);
+                    // 读取数据
+                    channel.read(buffer);
+                    // 切换读取模式
+                    buffer.flip();
+                    // 读取中文字符
+                    log.info("INFO READ:  {}",Charset.defaultCharset().decode(buffer));
+                }
+                selectionKey.cancel();
+            }
 
         }
     }
