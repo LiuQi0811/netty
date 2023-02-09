bae21b9e71f82b91ce815257781fb28eb5bc91fd
LIUQI <1139642444@qq.com>
2023-02-09 12:46:56 +0800

    EventLoopClient  channelFuture

--- src/main/java/com/liuqi/netty/c3/EventLoopServer.java
+++ src/main/java/com/liuqi/netty/c3/EventLoopServer.java
@@ -24,7 +24,7 @@ import java.nio.charset.Charset;
 public abstract class EventLoopServer {
 
     public static void main(String[] args) {
-        init();
+        channelFuture();
     }
     /**
      * 服务启动器初始化
@@ -56,4 +56,40 @@ public abstract class EventLoopServer {
                 //绑定 IP，端口
                 .bind(8080);
     }
+
+
+
+    /**
+     * channelFuture
+     * 负责组装 netty组件 启动服务器
+     * @return
+     */
+    public static ChannelFuture channelFuture(){
+        // 服务启动器 负责组装 netty组件 启动服务器
+        return new ServerBootstrap()
+                // Boss 和 Work
+                // Boss只负责ServerSocketChannel上 accept事件 Work只负责SocketChannel上的读写
+                // NioEventLoopGroup （selector,thread） 包含选择器和线程 ，group组
+                .group(new NioEventLoopGroup(),new NioEventLoopGroup())
+                // 选择 服务器的ServerSocketChannel 实现
+                .channel(NioServerSocketChannel.class)
+                // 一个负责处理连接 另一个负责处理读写，决定了 worker(child) 能执行哪些操作（handler）
+                .childHandler(
+                        //  channel 代表和客户端进行数据读写的通道，Initializer初始化，负责添加别的 handler
+                        new ChannelInitializer<NioSocketChannel>() {
+                            @Override
+                            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                                nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception { //读事件
+                                        final ByteBuf buf = (ByteBuf) msg;
+                                        log.info("buf读取信息 {}", buf.toString(Charset.defaultCharset()));
+                                    }
+                                });
+                            }
+                        })
+                //绑定 IP，端口
+                .bind(8080);
+    }
+
 }
